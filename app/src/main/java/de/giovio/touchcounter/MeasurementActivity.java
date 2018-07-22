0edd17f8a4eec6367460f61910783c8d52551842
PBerger <philipp.berger@rwth-aachen.de>
2018-07-22 16:26:31 +0200

    Fixed issue in layout

--- app/src/main/java/de/giovio/touchcounter/MeasurementActivity.java
+++ app/src/main/java/de/giovio/touchcounter/MeasurementActivity.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import java.util.Vector;
 
@@ -19,10 +21,17 @@ public class MeasurementActivity extends AppCompatActivity {
         setContentView(R.layout.activity_measurement);
 
         dataPoints = new Vector<>();
+        updateNumDataPointsCollected();
+    }
+
+    private void updateNumDataPointsCollected() {
+        final TextView textView = (TextView)findViewById(R.id.textNumDataPointsValue);
+        textView.setText(String.valueOf(dataPoints.size()));
     }
 
     public void btnMeasureOnClick(View v) {
         dataPoints.add(new DataPoint());
+        updateNumDataPointsCollected();
     }
 
     public void btnSaveOnClick(View v) {
@@ -35,18 +44,20 @@ public class MeasurementActivity extends AppCompatActivity {
             public void onClick(DialogInterface dialog, int which) {
                 switch (which){
                     case DialogInterface.BUTTON_POSITIVE:
-                        //Yes button clicked
+                        // Yes button clicked
+                        Intent intent = new Intent(MeasurementActivity.this, OverviewActivity.class);
+                        MeasurementActivity.this.startActivity(intent);
                         break;
 
                     case DialogInterface.BUTTON_NEGATIVE:
-                        //No button clicked
+                        // No button clicked
                         break;
                 }
             }
         };
 
-        AlertDialog.Builder builder = new AlertDialog.Builder(context);
-        builder.setMessage("Are you sure?").setPositiveButton("Yes", dialogClickListener)
-                .setNegativeButton("No", dialogClickListener).show();
+        AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+        builder.setMessage(getString(R.string.activity_measurement_dialog_cancel_text)).setPositiveButton(getString(R.string.activity_measurement_dialog_cancel_yes), dialogClickListener)
+                .setNegativeButton(getString(R.string.activity_measurement_dialog_cancel_no), dialogClickListener).show();
     }
 }
