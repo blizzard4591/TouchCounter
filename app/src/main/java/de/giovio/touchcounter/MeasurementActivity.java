48051be8caa6afcfb3f9479dfbdef2fd5e1066d8
PBerger <philipp.berger@rwth-aachen.de>
2018-07-22 15:19:46 +0200

    Rebuild stuff

---
+++ app/src/main/java/de/giovio/touchcounter/MeasurementActivity.java
@@ -0,0 +1,14 @@
+package de.giovio.touchcounter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MeasurementActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_measurement);
+    }
+}
